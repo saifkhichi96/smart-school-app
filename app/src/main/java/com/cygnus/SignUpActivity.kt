d10989f679154ae4e6fa533606b57856f72460d5
Muhammad Saif Ullah Khan <saifkhichi96@gmail.com>
2020-04-08 17:47:55 +0200

    Revert "Imported an external module with a custom view to create sign up forms"
    This reverts commit 5616b7173bd086cc2009d576bd0a5f9879c0682a.

--- app/src/main/java/com/cygnus/SignUpActivity.kt
+++ app/src/main/java/com/cygnus/SignUpActivity.kt
@@ -1,11 +1,8 @@
 package com.cygnus
 
 import android.os.Bundle
-import android.util.SparseArray
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import co.aspirasoft.view.WizardViewStep
-import kotlinx.android.synthetic.main.activity_sign_up.*
 
 
 /**
@@ -25,9 +22,6 @@ import kotlinx.android.synthetic.main.activity_sign_up.*
  */
 class SignUpActivity : AppCompatActivity() {
 
-    // TODO: Create sign up steps
-    private val steps: List<WizardViewStep> = listOf()
-
     lateinit var referralCode: String
     lateinit var accountType: String
     lateinit var emailLink: String
@@ -40,12 +34,6 @@ class SignUpActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
-
-        wizardView.setupWithWizardSteps(supportFragmentManager, steps)
-        wizardView.setOnSubmitListener {
-            // TODO: Handle form data
-        }
 
         // Read sign up parameters from intent
         referralCode = intent.getStringExtra(CygnusApp.EXTRA_REFERRAL_CODE) ?: ""
@@ -86,12 +74,6 @@ class SignUpActivity : AppCompatActivity() {
                 Toast.LENGTH_LONG
         ).show()
         finish()
-    }
-
-    override fun onBackPressed() {
-        if (!wizardView.onBackPressed()) {
-            super.onBackPressed()
-        }
     }
 
 }
