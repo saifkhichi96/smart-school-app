af67c905530d753d22d5ef605cd25b5f3c5cd7d0
Muhammad Saif Ullah Khan <saifkhichi96@gmail.com>
2020-04-19 19:39:43 +0200

    Homework sheets can also be uploaded/viewed now.

--- app/src/main/java/com/cygnus/SubjectActivity.kt
+++ app/src/main/java/com/cygnus/SubjectActivity.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
@@ -25,7 +26,7 @@ import com.cygnus.view.LectureView
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_subjects.*
+import kotlinx.android.synthetic.main.activity_subject.*
 
 
 /**
@@ -41,20 +42,26 @@ class SubjectActivity : DashboardChildActivity() {
 
     private var appointmentsAdapter: AppointmentsAdapter? = null
 
+    private lateinit var materialManager: FileManager
     private var materialAdapter: MaterialAdapter? = null
     private val material = ArrayList<CourseFile>()
 
-    private lateinit var mFileManager: FileManager
+    private lateinit var homeworkManager: FileManager
+    private var homeworkAdapter: MaterialAdapter? = null
+    private val homework = ArrayList<CourseFile>()
+
+    private var pickRequestCode = RESULT_ACTION_PICK_MATERIAL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_subjects)
+        setContentView(R.layout.activity_subject)
 
         subject = intent.getSerializableExtra(CygnusApp.EXTRA_SCHOOL_SUBJECT) as Subject? ?: return finish()
         editable = intent.getBooleanExtra(CygnusApp.EXTRA_EDITABLE_MODE, editable)
         if (editable) {
             addAppointmentButton.visibility = View.VISIBLE
             addMaterialButton.visibility = View.VISIBLE
+            addHomeworkButton.visibility = View.VISIBLE
         }
 
         addAppointmentButton.setOnClickListener {
@@ -75,33 +82,40 @@ class SubjectActivity : DashboardChildActivity() {
                             getString(R.string.permission_storage),
                             RC_WRITE_PERMISSION
                     )) {
-                pickFile()
+                pickFile(RESULT_ACTION_PICK_MATERIAL)
             }
         }
 
-        mFileManager = FileManager.newInstance(this, "$schoolId/${subject.classId}/${subject.name}/")
+        addHomeworkButton.setOnClickListener {
+            if (PermissionUtils.requestPermissionIfNeeded(
+                            this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            getString(R.string.permission_storage),
+                            RC_WRITE_PERMISSION
+                    )) {
+                pickFile(RESULT_ACTION_PICK_HOMEWORK)
+            }
+        }
+
+        materialManager = FileManager.newInstance(this, "$schoolId/courses/${subject.classId}/subjects/${subject.name}/lectures/")
+        homeworkManager = FileManager.newInstance(this, "$schoolId/courses/${subject.classId}/subjects/${subject.name}/exercises/")
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RESULT_ACTION_PICK && resultCode == Activity.RESULT_OK) {
-            data?.data?.getLastPathSegmentOnly(this)?.let { filename ->
-                val status = Snackbar.make(contentList, "Uploading...", Snackbar.LENGTH_INDEFINITE)
-                status.show()
-                mFileManager.upload(filename, data.data!!)
-                        .addOnSuccessListener {
-                            it.metadata?.let { metadata ->
-                                material.add(CourseFile(filename, metadata))
-                                materialAdapter?.notifyDataSetChanged()
-                            }
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                RESULT_ACTION_PICK_MATERIAL -> {
+                    data?.data?.getLastPathSegmentOnly(this)?.let { filename ->
+                        uploadFile(materialManager, filename, data.data!!, materialAdapter)
+                    }
+                }
 
-                            status.setText("File uploaded.")
-                            Handler().postDelayed({ status.dismiss() }, 2500L)
-                        }
-                        .addOnFailureListener {
-                            status.setText(it.message ?: "Failed to upload file.")
-                            Handler().postDelayed({ status.dismiss() }, 2500L)
-                        }
+                RESULT_ACTION_PICK_HOMEWORK -> {
+                    data?.data?.getLastPathSegmentOnly(this)?.let { filename ->
+                        uploadFile(homeworkManager, filename, data.data!!, homeworkAdapter)
+                    }
+                }
             }
         }
     }
@@ -110,7 +124,7 @@ class SubjectActivity : DashboardChildActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == RC_WRITE_PERMISSION) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                pickFile()
+                pickFile(pickRequestCode)
             }
         }
     }
@@ -129,16 +143,21 @@ class SubjectActivity : DashboardChildActivity() {
         appointmentsList.adapter = appointmentsAdapter
 
         // Show course material
-        showCourseMaterial()
+        showCourseContents()
     }
 
-    private fun showCourseMaterial() {
+    private fun showCourseContents() {
+        showClassMaterial()
+        showHomeworkExercises()
+    }
+
+    private fun showClassMaterial() {
         if (materialAdapter == null) {
-            materialAdapter = MaterialAdapter(this, material, mFileManager)
+            materialAdapter = MaterialAdapter(this, material, materialManager)
             contentList.adapter = materialAdapter
         }
 
-        mFileManager.listAll().addOnSuccessListener { result ->
+        materialManager.listAll().addOnSuccessListener { result ->
             material.clear()
             result?.items?.forEach { reference ->
                 reference.metadata.addOnSuccessListener { metadata ->
@@ -149,18 +168,57 @@ class SubjectActivity : DashboardChildActivity() {
         }
     }
 
-    private fun pickFile() {
+    private fun showHomeworkExercises() {
+        if (homeworkAdapter == null) {
+            homeworkAdapter = MaterialAdapter(this, homework, homeworkManager)
+            homeworkList.adapter = homeworkAdapter
+        }
+
+        homeworkManager.listAll().addOnSuccessListener { result ->
+            homework.clear()
+            result?.items?.forEach { reference ->
+                reference.metadata.addOnSuccessListener { metadata ->
+                    homework.add(CourseFile(reference.name, metadata))
+                    homeworkAdapter?.notifyDataSetChanged()
+                }
+            }
+        }
+    }
+
+    private fun pickFile(requestCode: Int) {
+        this.pickRequestCode = requestCode
+
         val i = Intent(Intent.ACTION_GET_CONTENT)
         i.type = "application/*"
         i.addCategory(Intent.CATEGORY_OPENABLE)
-        startActivityForResult(i, RESULT_ACTION_PICK)
+        startActivityForResult(i, requestCode)
+    }
+
+    private fun uploadFile(fm: FileManager, filename: String, data: Uri, adapter: MaterialAdapter?) {
+        val status = Snackbar.make(contentList, "Uploading...", Snackbar.LENGTH_INDEFINITE)
+        status.show()
+        fm.upload(filename, data)
+                .addOnSuccessListener {
+                    it.metadata?.let { metadata ->
+                        adapter?.add(CourseFile(filename, metadata))
+                        adapter?.notifyDataSetChanged()
+                    }
+
+                    status.setText("File uploaded.")
+                    Handler().postDelayed({ status.dismiss() }, 2500L)
+                }
+                .addOnFailureListener {
+                    status.setText(it.message ?: "Failed to uploadFile file.")
+                    Handler().postDelayed({ status.dismiss() }, 2500L)
+                }
     }
 
     private inner class AppointmentsAdapter(context: Context, lectures: List<Lecture>)
         : ModelViewAdapter<Lecture>(context, lectures, LectureView::class)
 
     companion object {
-        private const val RESULT_ACTION_PICK = 100
+        private const val RESULT_ACTION_PICK_MATERIAL = 100
+        private const val RESULT_ACTION_PICK_HOMEWORK = 150
         private const val RC_WRITE_PERMISSION = 200
     }
 
